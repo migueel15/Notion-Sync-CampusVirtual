1dbdfcb631a56915330504df60c521bed1fcf8f8
migueel15 <miguelangeldorado10@gmail.com>
2024-04-20 00:10:18 +0200

    Update Notion-Sync: sáb 20 abr 2024 00:10:18 CEST

--- src/config.js
+++ src/config.js
@@ -1,35 +1,35 @@
 // como maximo 60 dias
 const dateRange = {
-  startDate: 2,
-  endDate: 60,
+	startDate: 2,
+	endDate: 60,
 }
 
 // mapeo del nombre de las asignaturas
 // Nombre en CV : Nombre Custom Notion
 const asignaturas = {
-  FunEle: "Electronica",
-  "Cálculo para la Computación": "Calculo",
-  EstDat: "ED",
-  EstCom: "EC",
-  AnaDis: "ADA",
-  TeoAut: "TALF",
-  BasDat: "BD",
-  RedSis: "Redes",
-  SisInt: "SI",
-  SisOpe: "SO",
-  IntIng: "Software",
-  ProSisCon: "Concurrencia"
+	FunEle: "Electronica",
+	"Cálculo para la Computación": "Calculo",
+	EstDat: "ED",
+	EstCom: "EC",
+	AnaDis: "ADA",
+	TeoAut: "TALF",
+	BasDat: "BD",
+	RedSis: "Redes",
+	SisInt: "SI",
+	SisOpe: "SO",
+	IntIng: "Software",
+	ProSisCon: "Concurrencia"
 }
 
 // nombres de las propiedades de la base de datos en notion
 const propiedades = {
-  nombre: "Nombre de tarea", // nombre tarea
-  asignatura: "Asignatura", // select de asignaturas
-  descripcion: "Descripcion", // descripcion
-  fecha: "Fecha", // fecha
-  from: "From", // select (CV?)
-  cv: "cv-id", // plan text id de la tarea
-  tipo: "Tipo", // select (Tarea, Examen, etc)
+	nombre: "Nombre de tarea", // nombre tarea
+	asignatura: "Asignatura", // select de asignaturas
+	descripcion: "Descripcion", // descripcion
+	fecha: "Fecha", // fecha
+	from: "From", // select (CV?)
+	cv: "cv-id", // plan text id de la tarea
+	tipo: "Tipo", // select (Tarea, Examen, etc)
 }
 
 exports.dateRange = dateRange
--- src/utils.js
+++ src/utils.js
@@ -1,45 +1,45 @@
 const { asignaturas, dateRange } = require("./config")
 
 function mapAsignatura(string) {
-  let clave = ""
-  const claves = Object.keys(asignaturas)
-  for (let i = 0; i < claves.length; i++) {
-    if (string.includes(claves[i])) {
-      clave = claves[i]
-      return asignaturas[clave]
-    }
-  }
-  if (clave === "") return string
+	let clave = ""
+	const claves = Object.keys(asignaturas)
+	for (let i = 0; i < claves.length; i++) {
+		if (string.includes(claves[i])) {
+			clave = claves[i]
+			return asignaturas[clave]
+		}
+	}
+	if (clave === "") return string
 }
 
 function getDateRange() {
-  const today = new Date()
-  today.setHours(0, 0, 0, 0)
+	const today = new Date()
+	today.setHours(0, 0, 0, 0)
 
-  const minDate = new Date(today)
-  minDate.setDate(today.getDate() - dateRange.startDate)
+	const minDate = new Date(today)
+	minDate.setDate(today.getDate() - dateRange.startDate)
 
-  const maxDate = new Date(today)
-  maxDate.setDate(today.getDate() + dateRange.endDate - 1)
+	const maxDate = new Date(today)
+	maxDate.setDate(today.getDate() + dateRange.endDate - 1)
 
-  return {
-    minDate: minDate.toISOString(),
-    maxDate: maxDate.toISOString(),
-  }
+	return {
+		minDate: minDate.toISOString(),
+		maxDate: maxDate.toISOString(),
+	}
 }
 
 function fromUTCtoLocal(date) {
-  const timestamp = new Date(date)
-  const offset = (timestamp.getTimezoneOffset() / 60) * -1
-  timestamp.setHours(timestamp.getHours() + offset)
-  return timestamp
+	const timestamp = new Date(date)
+	const offset = (timestamp.getTimezoneOffset() / 60) * -1
+	timestamp.setHours(timestamp.getHours() + offset)
+	return timestamp
 }
 
 function fromLocalToUTC(date) {
-  const timestamp = new Date(date)
-  const offset = (timestamp.getTimezoneOffset() / 60) * -1
-  timestamp.setHours(timestamp.getHours() - offset)
-  return timestamp
+	const timestamp = new Date(date)
+	const offset = (timestamp.getTimezoneOffset() / 60) * -1
+	timestamp.setHours(timestamp.getHours() - offset)
+	return timestamp
 }
 
 module.exports = { mapAsignatura, getDateRange, fromUTCtoLocal, fromLocalToUTC }
