bfdb7f902a7ed61d001e60e4fc44e13abd252d08
migueel15 <miguelangeldorado10@gmail.com>
2024-09-06 17:19:00 +0200

    remove test

--- src/test.ts
+++
@@ -1,9 +0,0 @@
-import { getCvEvents } from "./ical.js";
-import dotenv from "dotenv"
-import { queryEventsFromNotion } from "./notion.js";
-dotenv.config()
-
-const cvE = await getCvEvents()
-const notionE = await queryEventsFromNotion()
-console.log(cvE)
-console.log(notionE)
