a718cc74075defe8adc7c01249ec51802178a2f5
migueel15 <miguelangeldorado10@gmail.com>
2025-09-24 21:20:18 +0200

    add big data

--- src/config.ts
+++ src/config.ts
@@ -18,7 +18,7 @@ export const asignaturas: { [longName: string]: string } = {
 	TeoAut: "TALF",
 	BasDat: "BD",
 	RedSis: "Redes",
-	IngSisIntDat: "Big Data!",
+	IngSisIntDat: "Big Data",
 	SisOpe: "SO",
 	IntIng: "Software",
 	ProSisCon: "Concurrencia",
