ee1de6cfe7fc6fdbdc84436bfe2b7e2bdc2f724f
migueel15 <miguelangeldorado10@gmail.com>
2026-03-16 21:02:17 +0100

    tareas con fecha por defecto

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,12 @@
-import './server.js'
-import './cron.js'
-import { logInfo } from './logger.js'
+import "./server.js";
+import "./cron.js";
+import { logInfo } from "./logger.js";
+import { syncAllUsers } from "./cron.js";
 
-logInfo('Notion Sync Multi-User Server Started', { context: 'bootstrap' })
-logInfo('API Server running with /sync-user endpoint', { context: 'bootstrap' })
-logInfo('Cron jobs scheduled for automatic syncing', { context: 'bootstrap' })
+logInfo("Notion Sync Multi-User Server Started", { context: "bootstrap" });
+logInfo("API Server running with /sync-user endpoint", {
+  context: "bootstrap",
+});
+logInfo("Cron jobs scheduled for automatic syncing", { context: "bootstrap" });
+
+await syncAllUsers();
